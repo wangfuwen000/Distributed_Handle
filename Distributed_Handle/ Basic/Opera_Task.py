79cddd0ffa012e4a8725536a6cc29a43e1a50851
shipYang <626999291@qq.com>
2019-07-18 16:20:57 +0800

    Update and rename Distributed_Handle/Basic/Opera_Task.py to  Distributed_Handle/ Basic/Opera_Task.py

--- Distributed_Handle/ Basic/Opera_Task.py
+++ Distributed_Handle/ Basic/Opera_Task.py
@@ -11,17 +11,15 @@ from Basic.Opera_Asyncio import Opera_Asynchronous
 
 class Asyncio_Task:
 
-    def asyncio_task(self):
-        bootstrap_servers = 'kafka2-test1.sinnet.huobiidc.com:9092'
-        topic_consumer = 'test-10_match_result_btcusdt'
-        topic_producer = 'test-10_match_result_htbtc'
-        group_id = 'test123'
+    def asyncio_task(self):   
+         group_id = 'test123'
+
+        bootstrap_servers = ''
+        topic_consumer = ''
+        topic_producer = ''
 
         ok = Opera_Kafka(bootstrap_servers, topic_consumer, group_id)
         oa = Opera_Asynchronous(ok.poll_persist_msg(topic_producer))
         of = Opera_File()
 
         oa.my_coroutine()
-
-
-
